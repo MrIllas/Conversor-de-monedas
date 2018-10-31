241500d980dd3dc6690c013711c515a54d3d29e3
Robert Recorda <alu2015324@a22o26.local>
2018-10-31 18:38:46 +0100

    Initial Commit

---
+++ Conversor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Conversor
+//
+//  Created by Robert Recorda on 31/10/18.
+//  Copyright © 2018 Robert Recorda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
